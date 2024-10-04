3c4c00ec81a430838f8e7e30995101d46142948d
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-10-04 18:13:10 +0200

    Add project files.

---
+++ Services/Catalog/Multishop.Catalog/Entities/ProductDetail.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Multishop.Catalog.Entities
+{
+    public class ProductDetail
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public  string ProductDetailID { get; set; }
+        public  string ProductDescription { get; set; }
+        public  string ProductInfo { get; set; }
+    }
+}
