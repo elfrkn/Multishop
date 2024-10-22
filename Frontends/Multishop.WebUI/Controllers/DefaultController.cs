6788b91481784e5f32cfc6b640d9c8b14c48cb76
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-10-22 18:51:39 +0200

    Kullanıcı arayüzü Layout

---
+++ Frontends/Multishop.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Multishop.WebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.directory1 = "MultiShop";
+            ViewBag.directory2 = "Ana Sayfa";
+            ViewBag.directory3 = "Ürün Listesi";
+            return View();
+
+        }
+    }
+}
