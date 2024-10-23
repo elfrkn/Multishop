b561f1cb7e4aa79a8c55378886da124e27ee643c
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-10-23 13:36:27 +0200

    Ürün Sayfası

---
+++ Frontends/Multishop.WebUI/Controllers/ProductListController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Multishop.WebUI.Controllers
+{
+    public class ProductListController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.directory1 = "Ana Sayfa";
+            ViewBag.directory2 = "Ürünler";
+            ViewBag.directory3 = "Ürün Listesi";
+
+            return View();
+        }
+
+        public IActionResult ProductDetail()
+        {
+            return View();
+        }
+    }
+}
