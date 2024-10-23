798becf79c39b1fc5c24d2f15871a8bcee95f68d
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-10-23 13:49:56 +0200

    Alışveriş Sepeti Sayfası Tasarım

---
+++ Frontends/Multishop.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Multishop.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.directory1 = "MultiShop";
+            ViewBag.directory2 = "İletişim";
+            ViewBag.directory3 = "Mesaj Gönder";
+
+            return View();
+        }
+    }
+}
